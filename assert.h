5a1a2d19ed95fe4746240344e716950a589d039a
Listeria monocytogenes <listeria@disroot.org>
2024-09-23 13:52:40 -0300

    move things around & update docs

--- assert.h
+++ assert.h
@@ -4,7 +4,7 @@
  *   STATIC_ASSERT(condition, message?): (may be a declaration)
  *     for static assertions at file, struct or function scope.
  *   STATIC_ASSERT_EXPR(condition, message?): (is a void expression)
- *     use this when an expression is required (i.e: in a switch statement).
+ *     use this when an expression is required.
  *   ASSERT(condition):
  *     for regular assertions, can be tuned for debugging by defining
  *     ASSERT_DEBUG or DEBUG, otherwise invokes undefined behavior on failure.
@@ -17,6 +17,21 @@
 #ifndef ASSERT_H_
 #define ASSERT_H_
 
+/* Interface */
+#ifndef STATIC_ASSERT
+# define STATIC_ASSERT(...) STATIC_ASSERT__IMPL(__VA_ARGS__, "", _)
+#endif
+
+#ifndef STATIC_ASSERT_EXPR
+# define STATIC_ASSERT_EXPR(...) STATIC_ASSERT_EXPR__IMPL(__VA_ARGS__, "", _)
+#endif
+
+#ifndef ASSERT
+# define ASSERT(condition) ((condition) ? (void)0 : ASSERT_FAILED)
+#endif
+
+
+/* Implementation */
 #if defined(__cpp_static_assert) || defined(static_assert) || \
     defined(__STDC_VERSION__) && __STDC_VERSION__ >= 202311L
 # define STATIC_ASSERT__IMPL(cond, msg, ...) static_assert(cond, msg)
@@ -41,38 +56,26 @@
      ((void)sizeof(int[1 - 2*(int)!(condition)]))
 #endif
 
-#ifndef STATIC_ASSERT
-# define STATIC_ASSERT(...) STATIC_ASSERT__IMPL(__VA_ARGS__, "", _)
-#endif
-
-#ifndef STATIC_ASSERT_EXPR
-# define STATIC_ASSERT_EXPR(...) STATIC_ASSERT_EXPR__IMPL(__VA_ARGS__, "", _)
-#endif
-
-#ifndef ASSERT
-# ifndef ASSERT_FAILED
-#  if defined(ASSERT_DEBUG) || defined(DEBUG)
-#   if defined(__GNUC__)
-#    define ASSERT_FAILED __builtin_trap()
-#   elif defined(_MSC_VER)
-#    define ASSERT_FAILED __debugbreak()
-#   else
-#    define ASSERT_FAILED ((void)(*(volatile int *)0 = 0))
-#   endif
+#ifndef ASSERT_FAILED
+# if defined(ASSERT_DEBUG) || defined(DEBUG)
+#  if defined(__GNUC__)
+#   define ASSERT_FAILED __builtin_trap()
+#  elif defined(_MSC_VER)
+#   define ASSERT_FAILED __debugbreak()
 #  else
-#   if defined(unreachable)
-#    define ASSERT_FAILED unreachable()
-#   elif defined(__GNUC__)
-#    define ASSERT_FAILED __builtin_unreachable()
-#   elif defined(_MSC_VER)
-#    define ASSERT_FAILED __assume(0)
-#   else /* undefined behavior, should be optimized out */
-#    define ASSERT_FAILED ((void)(*(int*)0 = 0))
-#   endif
+#   define ASSERT_FAILED ((void)(*(volatile int *)0 = 0))
+#  endif
+# else
+#  if defined(unreachable)
+#   define ASSERT_FAILED unreachable()
+#  elif defined(__GNUC__)
+#   define ASSERT_FAILED __builtin_unreachable()
+#  elif defined(_MSC_VER)
+#   define ASSERT_FAILED __assume(0)
+#  else /* undefined behavior, should be optimized out */
+#   define ASSERT_FAILED ((void)(*(int*)0 = 0))
 #  endif
 # endif
-#
-# define ASSERT(condition) ((condition) ? (void)0 : ASSERT_FAILED)
 #endif
 
 #endif
